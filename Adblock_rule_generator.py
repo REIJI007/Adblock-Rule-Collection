e5f0f9d242cad71be29d7e435f3f52fcb839f8b0
REIJI007 <miyajimareji007@gmail.com>
2024-08-22 07:13:31 +0800

    Update Adblock_rule_generator.py
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- Adblock_rule_generator.py
+++ Adblock_rule_generator.py
@@ -67,7 +67,8 @@ filter_urls = [
     "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt",
     "https://raw.githubusercontent.com/Lynricsy/HyperADRules/master/rules.txt",
     "https://raw.githubusercontent.com/Lynricsy/HyperADRules/master/dns.txt",
-    "https://raw.githubusercontent.com/guandasheng/adguardhome/main/rule/all.txt"
+    "https://raw.githubusercontent.com/guandasheng/adguardhome/main/rule/all.txt",
+    "https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/rule.txt"
 ]
 
 # 保存路径设定为当前工作目录的根目录下，并命名为 'ADBLOCK_RULE_COLLECTION.txt'
