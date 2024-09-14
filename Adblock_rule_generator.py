f52459293a27f20fb37dea984193ac1d156f715e
REIJI007 <miyajimareji007@gmail.com>
2024-09-14 12:26:53 +0800

    Update Adblock_rule_generator.py
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- Adblock_rule_generator.py
+++ Adblock_rule_generator.py
@@ -29,9 +29,9 @@ install_packages(required_packages)
 
 warnings.simplefilter('ignore', InsecureRequestWarning)
 
-# 过滤器 URL 列表（省略实际 URL）
+# 过滤器 URL 列表
 filter_urls = [
-        "https://anti-ad.net/adguard.txt",
+    "https://anti-ad.net/adguard.txt",
     "https://anti-ad.net/easylist.txt",
     "https://big.oisd.nl",
     "https://easylist.to/easylist/easylist.txt",
